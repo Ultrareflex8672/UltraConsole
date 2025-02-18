2588c7c6a9440fa1adaefd4b307d1b3fae447b99
Ultrareflex8672 <ultrareflex@ultrareflex.com>
2025-02-19 01:46:29 +0300

    Modül Hatası Uyarıları Eklendi

--- application/modul_olusturucu.py
+++ application/modul_olusturucu.py
@@ -8,8 +8,9 @@ class ModuleLoader(ConfigHandler):
         module_path = os.path.join("modules", f"{func_name}.py")
 
         if not os.path.exists(module_path):
-            print(f"Fonksiyon bulunamadı: {func_name}.py")
-            return
+            message = f"Modül dosyası bulunamadı: {func_name}.py"
+            print(message)
+            return message
         
         spec = importlib.util.spec_from_file_location(func_name, module_path)
         module = importlib.util.module_from_spec(spec)
@@ -19,4 +20,6 @@ class ModuleLoader(ConfigHandler):
             func = getattr(module, func_name)
             return func(selection)
         else:
-            print(f"{func_name} fonksiyonu {func_name}.py içinde tanımlı değil.")
+            message = f"{func_name} fonksiyonu {func_name}.py modülü içinde tanımlı değil."
+            print(message)
+            return message
