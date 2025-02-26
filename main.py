557973afe68ca03196f3d536718c66f956e23c1b
Ultrareflex8672 <ultrareflex@ultrareflex.com>
2025-02-27 01:46:33 +0300

    Yazım Düzeltme

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from application.kullanici_kontrol import User         # UltraConsole Developed by Ultrareflex (Kemal Burak YILDIRIM)
 
 if __name__ == "__main__":                             # Programın ana modül olup olmadığını kontrol et
- User().__init__()                                     # Kullanıcı sınıfını yükle
+    User()                                             # Kullanıcı sınıfını yükle
  
 
     
